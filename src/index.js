d82c253e5b7ee10c28bc8205adbd43b561435f02
Quasar <Axzurey@gmail.com>
2021-04-29 11:20:37 -0400

    e

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const {app, BrowserWindow, Tray, Menu, nativeImage} = require('electron')
 
+/*
+<script src="https://preview.babylonjs.com/babylon.js"></script>
+        <script src="https://preview.babylonjs.com/loaders/babylonjs.loaders.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/babylon-vrm-loader@^1.4/dist/index.js"></script>
+*/
+
 function LoadWindowAsync() {
     Window = new BrowserWindow({
         width : 1000,
@@ -11,7 +17,6 @@ function LoadWindowAsync() {
             enableRemoteModule : true
         },
         title : "Maid",
-        icon : "./maika.png",
         titlebarstyle : "hidden",
         darkTheme: true
     })
